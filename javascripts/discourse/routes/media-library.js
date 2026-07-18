714eafa0e606461c62116ad9467e8d552bc14744
heartbeatpleasure <248991679+heartbeatpleasure@users.noreply.github.com>
2026-07-18 03:28:01 +0200

    Resolve browser title as Media Library

--- javascripts/discourse/routes/media-library.js
+++ javascripts/discourse/routes/media-library.js
@@ -1,8 +1,9 @@
 // javascripts/discourse/routes/media-library.js
 import DiscourseRoute from "discourse/routes/discourse";
+import I18n from "I18n";
 
 export default class MediaLibraryRoute extends DiscourseRoute {
   titleToken() {
-    return "media_gallery.title";
+    return I18n.t("media_gallery.title");
   }
 }
